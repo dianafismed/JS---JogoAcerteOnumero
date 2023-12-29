c551875a50d81028579c3499d3e976b17a267124
Diana Avila <45690382+dianafismed@users.noreply.github.com>
2023-12-29 18:01:10 -0300

    up

--- app/validacao.js
+++ app/validacao.js
@@ -17,6 +17,7 @@ function verifica(chute) {
     document.body.innerHTML = `
     <h2>Você acertou !!!</h2>
     <h3>O número secreto era ${numeroSecreto}</h3>
+    <button id='btn-jogar' class='jogar'> Jogar novamente </button>
     `
   }else if(numero > numeroSecreto){
     msg.innerHTML = `<div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>`
@@ -32,3 +33,8 @@ function maiorOuMenor(numero) {
   return numero > maiorValor || numero < menorValor
 }
 
+document.body.addEventListener('click', e =>{
+  if (e.target.id == 'btn-jogar'){
+    window.location.reload()
+  }
+})
